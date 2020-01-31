d13aab6b5b1bfe797bc1aa7e307668405038358e
KUMN <KUMN@users.noreply.github.com>
2020-01-30 22:17:06 -0800

    Add files via upload

--- leet_twoSum.py
+++ leet_twoSum.py
@@ -49,6 +49,7 @@ def twoSum(nums, target):
     return result
 
 
+#Brute force to linearly search the list with 2 loops and then call the index function of list
 #Time limit exceeded
 def threeSum_slower(self, nums: List[int]) -> List[List[int]]:
         l = len(nums)
@@ -58,7 +59,7 @@ def threeSum_slower(self, nums: List[int]) -> List[List[int]]:
             for j in range(i+1, l):
                 try:
                     k = nums[j+1:].index(0-(nums[i]+nums[j]))
-                    if (not (set([nums[i], nums[j], nums[k+j+1]]) in ref)):
+                    if (not (set([nums[i], nums[j], nums[k+j+1]]) in ref)): #check for duplicates
                         result.append([nums[i], nums[j], nums[k+j+1]])
                         ref.append(set([nums[i], nums[j], nums[k+j+1]]))
                 except:
@@ -66,9 +67,12 @@ def threeSum_slower(self, nums: List[int]) -> List[List[int]]:
         return result
 
 
-
+#Similar to above but the second loop is replaced with a function call to twoSum
+#The first loop is a linear search, so is the second one within the function twoSum
+#For a given number in i, call a function to pick all 2 pairs of numbers from the rest of the list that sum to 0
+#While appending to the main result set check for no duplicates in the results
 #Time limit exceeded
-def threeSum2(nums):
+def threeSum3(nums):
     def twoSum3(nums, target):
         l=len(nums)
         result=[]
@@ -90,12 +94,16 @@ def threeSum2(nums):
     for ix in range(l3):
         r1 = twoSum3(nums[ix+1:], 0-nums[ix])
         for x in r1:
-            if not (set([nums[ix], x[0], x[1]]) in ref):
+            if not (set([nums[ix], x[0], x[1]]) in ref): #check for duplicates
                 result3.append([nums[ix], x[0], x[1]])
                 ref.append(set([nums[ix], x[0], x[1]]))
     return result3
         
-def threeSum(nums):
+#The list is first sorted and then there is a linear search with 2 loops and third is index call on list
+#Check if the next number in loop 1 or loop2 is same as earlier one, in which case avoid the loop
+#Thus no duplicates are created. There is then no need for the code to check duplicates
+#The second loop is done with a function call to twoSum. Thus we have to loop through twoSum's results.
+def threeSum2(nums):
     def twoSum3(nums, target):
         l=len(nums)
         result=[]
@@ -121,4 +129,60 @@ def threeSum(nums):
 
     return result3
 
+#Sort the list.
+#Linear search in 2 loops and then call index function to get the number list
+#If the number is same as the prior one in the loop then skip it
+#Thus duplicates are not created
+#Also there is no function call to twoSum so the results from it do not need to looped through either
+#Entries are made directly to the main results list
+
+def threeSum1(self, nums: List[int]) -> List[List[int]]:
+        result3=[]
+        l3=len(nums)
+        nums.sort()
+        for ix in range(l3):
+            if ix>0 and nums[ix]==nums[ix-1]:
+                    continue
     
+            for i in range(len(nums[ix+1:])):
+                if i>0 and nums[i+ix+1]==nums[i+ix]:
+                    continue
+                try:
+                    j=nums[i+1+ix+1:].index(-nums[ix]-nums[i+ix+1])
+                    result3.append([nums[ix], nums[i+ix+1], nums[j+i+1+ix+1]])
+                except:
+                    continue
+
+        return result3
+
+
+#Sort the list
+#Linear search with 2 loops and in the third one do a binary search for the value rather than calling index function of the list
+#No functions are called so no need to loop through results to create the result set
+#Entries are made directly to results
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    result3=[]
+    l3=len(nums)
+    nums.sort()
+    for ix in range(l3):
+        if ix>0 and nums[ix]==nums[ix-1]:
+            continue
+    
+        for i in range(len(nums[ix+1:])):
+            if i>0 and nums[i+ix+1]==nums[i+ix]:
+                continue
+            #j=nums[i+1+ix+1:].index(-nums[ix]-nums[i+ix+1])
+            st=i+1+ix+1
+            end=l3-1
+            value=-nums[ix]-nums[i+ix+1]
+            while end-st >=0:
+                mid=int((end+st)/2)
+                if nums[mid]==value:
+                    result3.append([nums[ix], nums[i+ix+1], nums[mid]])
+                    break
+                elif value<nums[mid]:
+                    end=mid-1
+                else:
+                    st=mid+1
+
+    return result3
---
+++ udacity_merge_sort.py
@@ -0,0 +1,69 @@
+"""Implement a function recursively to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions."""
+
+def get_fib(position):
+    if position<=0:
+        return 0
+    elif position==1:
+        return 1
+    else:
+        return get_fib(position-1) + get_fib(position-2)
+
+def get_fib(position):
+    if position<=1:
+        return position
+    else:
+        return get_fib(position-1) + get_fib(position-2)
+
+#Implement fibonacci series with a loop
+def get_fib(pos):
+    prev=0
+    curr=1
+    if pos==0:
+        return prev
+    if pos==1:
+        return curr
+    for i in range(2,pos+1):
+        val=prev+curr
+        prev=curr
+        curr=val
+    return val
+
+#Implement fibonacci series by copying prior values in a hash map
+
+#Implement merge sort
+#Merge Sort
+def merge_sort(input_list):
+    output_list=[]
+    l=len(input_list)
+    if l <=1:
+        output_list=input_list
+    else:
+        mid=int(l/2)
+        left_half=merge_sort(input_list[:mid])
+        right_half=merge_sort(input_list[mid:])
+        ml=len(left_half) + len(right_half)
+        i,j=0,0
+        l_elem=left_half[i]
+        r_elem=right_half[j]
+        while True:
+            if l_elem<r_elem:
+                output_list.append(l_elem)
+                i+=1
+                try:
+                    l_elem=left_half[i]
+                except:
+                    output_list=output_list+right_half[j:]
+                    break
+            else:
+                output_list.append(r_elem)
+                j+=1
+                try:
+                    r_elem=right_half[j]
+                except:
+                    output_list=output_list+left_half[i:]
+                    break
+    return output_list
+
